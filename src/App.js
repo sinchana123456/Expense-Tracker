ad3626e8ba97b55707c36b14c9433dd02af74da2
Sinchana Gowda <113234656+sinchana123456@users.noreply.github.com>
2023-01-19 19:29:58 +0530

    Creating Signup Screen

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Fragment } from 'react';
+import Authentication from './components/Authentication/Authentication';
+
+function App() {
+  return (
+    <Fragment>
+      <Authentication />
+    </Fragment>
+  );
+}
+
+export default App;
